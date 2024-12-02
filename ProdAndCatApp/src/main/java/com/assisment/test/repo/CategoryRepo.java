67b6672b1cba625f080c4d63b06ff794110b9943
bablue9775 <vishnusarang04@gmail.com>
2024-12-02 14:10:54 +0530

    Delete ProdAndCatApp directory

--- ProdAndCatApp/src/main/java/com/assisment/test/repo/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.assisment.test.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.assisment.test.entities.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
-
-}
